f1a25579b29201e66ba7d326ba10bbe2c9f0b4d1
Petr Svarovsky <petr.svarovsky@greps.com>
2021-10-01 18:46:03 +0200

    Initial Commit

---
+++ SpaceX/SpaceXApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpaceXApp.swift
+//  SpaceX
+//
+//  Created by Twice Upon on 01/10/2021.
+//
+
+import SwiftUI
+
+@main
+struct SpaceXApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
